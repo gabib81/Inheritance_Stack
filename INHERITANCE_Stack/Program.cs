9a2f15179f8536c6966f4e05687b166577795c3b
gabi.b <gabi.b@lsports.eu>
2021-08-03 11:07:55 +0300

    Add project files.

---
+++ INHERITANCE_Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace INHERITANCE_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var stack = new Stack();
+             stack.Push(22.54);
+             //stack.Push(null);
+             stack.Push("Gabi");
+             stack.Push(DateTime.Now);
+
+             //stack.Clear();
+
+             Console.WriteLine(stack.Pop());
+             Console.WriteLine(stack.Pop());
+             Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
---
+++ INHERITANCE_Stack/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace INHERITANCE_Stack
+{
+    public class Stack
+    {
+        private readonly ArrayList _stackObjects = new ArrayList();
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException();
+            _stackObjects.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_stackObjects.Count == 0)
+                throw new InvalidOperationException();
+            var item = _stackObjects[^1];
+            _stackObjects.RemoveAt(_stackObjects.Count - 1);
+            return item;
+        }
+
+        public void Clear()
+        {
+            _stackObjects.Clear();
+        }
+    }
+}
